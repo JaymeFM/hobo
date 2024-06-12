067bbb23ead96bb41aa4024e2cebc67c5a43fff8
Jayme <105801777+JaymeFM@users.noreply.github.com>
2024-06-12 11:25:25 +0200

    wow super coool search function enz

--- backend/search.php
+++ backend/search.php
@@ -12,51 +12,78 @@ require_once "database.php";
 $database = new Database("localhost", "root", "", "hobo");
 
 // GET DATA
-function getSerie($connection) {
-    $serieId = intval($_GET["serieId"]);
+function getData($connection, $search, $genre) {
+    $sql = "SELECT serie.SerieID, serie.SerieTitel, GROUP_CONCAT(genre.GenreNaam SEPARATOR ', ') AS GenreNamen
+            FROM serie
+            LEFT JOIN serie_genre ON serie.SerieID = serie_genre.SerieID
+            LEFT JOIN genre ON serie_genre.GenreID = genre.GenreID
+            WHERE Actief=1";
 
-    $sql = "SELECT * FROM serie WHERE SerieID = :serieId";
-    $stmt = $connection->prepare($sql);
-    $stmt->bindValue(':serieId', $serieId, PDO::PARAM_STR);
-
-    $stmt->execute();
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    return $results;
-}
-
-function getEpisodes($connection, $seasonId) {
-    $sql = "SELECT * FROM aflevering WHERE SeizID = :seasonId";
-    $stmt = $connection->prepare($sql);
-    $stmt->bindValue(':seasonId', $seasonId, PDO::PARAM_STR);
-
-    $stmt->execute();
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    return $results;
-}
-
-function getSeasons($connection) {
-    $serieId = intval($_GET["serieId"]);
-
-    $sql = "SELECT * FROM seizoen WHERE SerieID = :serieId";
-    $stmt = $connection->prepare($sql);
-    $stmt->bindValue(':serieId', $serieId, PDO::PARAM_STR);
-
-    $stmt->execute();
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    foreach ($results as $key => $value) {
-        $results[$key]["Episodes"] = getEpisodes($connection, $value['SeizoenID']);
+    if (!empty($search)) {
+        $sql .= " AND serie.SerieTitel LIKE :name";
     }
 
+    if (!empty($genre)) {
+        $sql .= " AND genre.GenreNaam LIKE :genre";
+    }
+
+    $sql .= " GROUP BY serie.SerieTitel";
+
+    $stmt = $connection->prepare($sql);
+
+    if (!empty($search)) {
+        $stmt->bindValue(':name', "%$search%", PDO::PARAM_STR);
+    }
+
+    if (!empty($genre)) {
+        $stmt->bindValue(':genre', "%$genre%", PDO::PARAM_STR);
+    }
+    
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
     return $results;
 }
 
 // RETURN RESULTS
-$results = (object) [
-    'serie' => getSerie($database->connection),
-    'seasons' => getSeasons($database->connection),
-];
-
+$entityBody = json_decode(file_get_contents('php://input'), true);
+$results = getData($database->connection, $entityBody['search'], $entityBody['genre']);
 echo json_encode($results);
+
+
+// function searchItems($connection, $name_query, $genre_query) {
+//     $sql = "SELECT serie.SerieTitel, GROUP_CONCAT(genre.GenreNaam SEPARATOR ', ') AS GenreNamen
+//             FROM serie
+//             LEFT JOIN serie_genre ON serie.SerieID = serie_genre.SerieID
+//             LEFT JOIN genre ON serie_genre.GenreID = genre.GenreID
+//             WHERE 1=1";
+
+//     if (!empty($name_query)) {
+//         $sql .= " AND serie.SerieTitel LIKE :name";
+//     }
+
+//     if (!empty($genre_query)) {
+//         $sql .= " AND genre.GenreNaam LIKE :genre";
+//     }
+
+//     $sql .= " GROUP BY serie.SerieTitel";
+
+//     $stmt = $connection->prepare($sql);
+
+//     if (!empty($name_query)) {
+//         $stmt->bindValue(':name', "%$name_query%", PDO::PARAM_STR);
+//     }
+
+//     if (!empty($genre_query)) {
+//         $stmt->bindValue(':genre', "%$genre_query%", PDO::PARAM_STR);
+//     }
+
+//     $stmt->execute();
+//     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//     return $results;
+// }
+
+// echo searchItems($database->connection, $_GET['search'], null)
+
+?>
