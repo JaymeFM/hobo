d92b130e906b6cb4cbc1f8281450966ffc1c395b
Jayme <jaymekerkvliet@outlook.com>
2024-06-15 12:17:38 +0200

    Update HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,7 +4,7 @@
     import { reactive } from 'vue';
     import Cookies from 'js-cookie';
 
-    const serverData = reactive({genres: {}, new: {}});
+    const serverData = reactive({genres: {}, new: {}, continue: {}});
 
     const userData = storeToRefs(userStore());
 
@@ -92,7 +92,51 @@
             serverData.new.pages[page].push(serie)
         }
 
-        console.log(serverData)
+        console.log(fetchedData.continue)
+        serverData.continue = {used: {}, list: [], pages: {}, page: '0'}
+        for(const serie of fetchedData.continue) {
+            var serieId = serie.SerieID
+            var imageId = ""
+
+            if(serieId < 10000) {
+                imageId += "0"
+            }
+
+            if(serieId < 1000) {
+                imageId += "0"
+            }
+
+            if(serieId < 100) {
+                imageId += "0"
+            }
+            
+            if(serieId < 10) {
+                imageId += "0"
+            }
+
+            imageId += serieId
+
+            serie.ImageID = imageId
+            
+            if(!serverData.continue.used[serieId]) {
+                serverData.continue.used[serieId] = true
+                serverData.continue.list.push(serie)
+            }
+        }
+
+        for(const serieNumber in serverData.continue.list) {
+            var serie = serverData.continue.list[serieNumber]
+
+            let page = Math.floor(serieNumber / 6)
+
+            if (!serverData.continue.pages[page]) {
+                serverData.continue.pages[page] = []
+            }
+
+            serverData.continue.pages[page].push(serie)
+        }
+        
+        console.log(serverData.continue)
     } 
 
     getServerData()
@@ -101,6 +145,27 @@
 
 <template>
     <div>
+        <div>
+            <div class="title">Continue Watching</div>
+            <q-carousel
+                v-model="serverData.continue.page"
+                transition-prev="slide-right"
+                transition-next="slide-left"
+                swipeable
+                animated
+                control-color="white"
+                arrows
+                class="bg-dark"
+            >
+                <q-carousel-slide 
+                v-for="(page, PageID) in serverData.continue.pages" :key="PageID"
+                :name="PageID" class="row q-col-gutter-sm">
+                    <router-link v-for="(serie, SerieNumber) in page" :key="SerieNumber" class="col-2" :to="userData.loggedIn.value ? `serie/${serie.SerieID}` : '../login'">
+                        <img :src="`serieImages/${serie.ImageID}.jpg`">
+                    </router-link>
+                </q-carousel-slide>
+            </q-carousel>
+        </div>
         <div>
             <div class="title">New</div>
             <q-carousel
